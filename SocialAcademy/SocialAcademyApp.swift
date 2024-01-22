39e597d29bf5ad7901594ba87adec422e2dd0e97
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2024-01-22 11:05:08 -0700

    Create Project and Connect to Firebase

--- SocialAcademy/SocialAcademyApp.swift
+++ SocialAcademy/SocialAcademyApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct SocialAcademyApp: App {
+    
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//    func application (_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+//        
+//        FirebaseApp.configure()
+//        
+//        return true
+//    }
+//}
