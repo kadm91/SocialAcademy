f6c73210430611b0844e7fec385f5fe0c1a01c76
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2024-01-22 10:33:44 -0700

    Initial Commit

---
+++ SocialAcademy/SocialAcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialAcademyApp.swift
+//  SocialAcademy
+//
+//  Created by Kevin Martinez on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialAcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
